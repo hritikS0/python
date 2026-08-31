8deb706162b08aa2055cd92a9c44d57f7306d638
Hritik Sharma <168188588+hritikS0@users.noreply.github.com>
2026-08-31 18:56:44 +0000

    initial commit

---
+++ functions-variable.py
@@ -0,0 +1,12 @@
+print("hello world");
+# functions 
+# functions ->   Functions are reusable blocks of code designed to 
+#                perform a specific task, helping to organize programs
+#                into smaller , manageable sections and reducing code duplication.
+
+input("What is your name? "); 
+#built-in function that captures user input from the keyboard, displaying an optional prompt message and waiting for the user to press Enter.  
+#It always returns the entered data as a string, regardless of whether the user types numbers, text, or symbols. 
+
+
+#  Return values 
