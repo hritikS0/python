a5f180a033be8f5f3989a59669f9ccc19da42746
Hritik Sharma <168188588+hritikS0@users.noreply.github.com>
2026-09-01 05:29:07 +0000

    learned functions and diff types of variable

---
+++ calculator.py
@@ -0,0 +1,7 @@
+#x = int(input("Enter your first number"));
+#y = int(input("Enter your second number"));
+# float
+#x = float(input("Enter your first number " ));
+#y = float(input("Enter your second number " ));
+
+#print(round(x/y,2)) 
--- functions-variable.py
+++ functions-variable.py
@@ -1,12 +1,43 @@
-print("hello world");
+#print("hello world");
 # functions 
 # functions ->   Functions are reusable blocks of code designed to 
-#                perform a specific task, helping to organize programs
 #                into smaller , manageable sections and reducing code duplication.
+#                perform a specific task, helping to organize programs
 
-input("What is your name? "); 
+#name = input("What is your name? ").strip().title();
 #built-in function that captures user input from the keyboard, displaying an optional prompt message and waiting for the user to press Enter.  
 #It always returns the entered data as a string, regardless of whether the user types numbers, text, or symbols. 
 
+# String methods
 
-#  Return values 
+# to remove whitespace from string
+#name = name.strip(); 
+
+# Capitalize users name
+#name = name.capitalize()
+
+#
+#name = name.strip().title();
+
+# split users name into first name and lastname
+#first, last = name.split(" ");
+
+#print("heloo "+ name);
+#print("hello", sep="???");
+#print('hello, "friend" ');
+#print(f"hello,{name}");
+#print(first , last)
+
+
+
+# Functions 
+
+def main():
+    name = input("What is your name? ")
+    hello(name)
+
+def hello(name="World"):
+    print("Hello, ",name)
+
+
+main()
