fcd2a1be5ba846d7e51979e92119742ad9f0ef45
Hritik Sharma <168188588+hritikS0@users.noreply.github.com>
2026-09-01 11:36:16 +0000

    libraries intro

---
+++ lib.py
@@ -0,0 +1,16 @@
+import statistics
+
+avg = statistics.mean([100,90,100,20,100,30])
+print(avg)
+# import random
+# # from random import choice
+
+# # coin = choice(['heads','tails'])
+# # print(coin)
+
+# number = random.randint(1,10)
+# print(number)
+# cards = ["jack", "queen","king"];
+# random.shuffle(cards)
+# for card in cards:
+#     print(card)
