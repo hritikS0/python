b1400f9c0fa0c55e49b526338bd7e49f124b4604
hritikS0 <sharmahritik8077@gmail.com>
2026-09-08 16:21:30 +0530

    oops

---
+++ bs.py
@@ -0,0 +1,96 @@
+# name = "hritik"
+# age = 22
+# height = '5"9'
+# print(f"Hello my name is {name} and my age is {age}, I am a student");
+
+
+# Swap variables
+# a = 10
+# b = 20
+# we use tuple unpacking
+# a , b = b ,a
+# print(a,b)
+
+
+# Temperature converter
+# def tempConverter():
+#     c = 25
+#     tempf = (c * 9/5) + 32
+#     print(tempf)
+# tempConverter()
+
+#is even
+# def is_even(n):
+#     return n%2==0
+# print(is_even(13))
+
+
+#Positive, negative, or zero
+
+# def deter(n):
+#     if n > 0:
+#         return "Positive"
+#     elif n < 0:
+#         return "negative"
+#     else:
+#         return "zero"
+# print(deter(0))
+
+
+
+# Maximum of three
+# def find_max(a,b,c):
+#     if a > b and a > c:
+#         print("a is greater")
+#     elif b > a and b > c:
+#         print("b is greater")
+#     else :
+#         print("c is greater")
+
+# find_max(10,222,3)
+
+#  Grade calculator
+
+# def gradeCal(marks):
+#     if marks < 60:
+#         print("F")
+#     elif marks < 70:
+#         print("D")
+#     elif marks < 80:
+#         print("C")
+#     elif marks < 90 :
+#         print("B")
+#     else:
+#         print("A")
+
+
+# gradeCal(76)
+
+
+# 8. Leap year
+# todo
+#
+
+
+
+# Level 2 — Strings
+# 9. Reverse a string
+
+# Given:
+
+# s = "python"
+
+# Output:
+
+# "nohtyp"
+
+# Try three ways:
+
+# loop
+# slicing
+# reversed()
+
+s = "python"
+print(s[:-1])
+# for char in s:
+#     print(char)
--- oops.py
+++ oops.py
@@ -1,14 +1,142 @@
-def main():
-    student = get_student()
-    if student[0] == "padma":
-        student[1] = "RavenClaw"
-    print(f"{student[0]} from {student[1]}")
+# class Student:
+#     def __init__(self,name,house):
+#         if not name:
+#             raise ValueError("Missing Name")
+#         if house not in ["1","2"]:
+#             raise ValueError("Invalid House")
+#         self.name = name
+#         self.house = house
 
 
-def get_student():
-    name = input("name : ")
-    house =  input("House : ")
-    return [name,house]
+# def main():
+#     student = get_student()
+#     print(f"{student.name} from {student.house}")
 
-if __name__ == "__main__":
-    main()
+# def get_student():
+#     name =  input("Name: ")
+#     house = input("House: ")
+#     return Student(name,house) # contructer call 
+# # Getter
+# def house(self):
+#     return self.house
+
+# #  Setter
+# def house(self,house):
+#     self.house = house
+#     # student = Student()
+#     # student.name = input("name: ")
+#     # student.house = input("House: ")
+#     # student = {}
+#     # student["name"] = input("Name: ")
+#     # student["house"] = input("House: ")
+#     # name =  input("Name: ")
+#     # house = input("House: ")
+#     # return {
+#     #     "name": name ,"house":house
+#     # }
+
+# if __name__ == "__main__":
+#     main()     
+
+
+
+
+
+
+
+
+class Car:
+    total_car = 0
+
+    def __init__(self,brand,model):  # we call init contructer
+        self.__brand = brand
+        self.__model = model
+        Car.total_car += 1        
+
+    def full_Name(self):
+        return f"{self.__brand} {self.__model} "
+
+    def get_brand(self):
+        return self.__brand + "! "
+
+    def fuel_type(self):
+        return "PETROL OR DIESEL"
+    
+    @staticmethod
+    def general_info():
+        return "Cars are means of transport"
+    @property
+    def model(self):
+        return self.__model
+class ElectricCar(Car):
+    def __init__(self, brand, model,battery_size):
+        super().__init__(brand,model)
+        self.battery_size = battery_size
+
+    def fuel_type(self):
+        return "Electric Charge"
+
+
+class Battery:
+    def battery_info(self):
+        return "this is battery"
+
+class Engine:
+    def engine_info(self):
+        return "this is engine info"
+class ElectricCar2(Battery,Engine,Car):
+    pass
+
+
+
+my_new_tesla = ElectricCar2("Tesla","model S")
+print(my_new_tesla.battery_info())
+print(my_new_tesla.engine_info())
+
+# my_car = Car("Tata","Safari")
+# my_car.model = "Model S"
+# print(my_car.model())
+# print(my_car.general_info())
+# print(my_car.general_info())
+# print(Car.general_info())
+
+
+
+
+
+my_tesla = ElectricCar("Tesla" , "Model S", "85KWH")
+# print(my_tesla.get_brand())
+# print(my_tesla.__brand) # this will give error because brand is private and we can not access it directly
+# print(my_tesla.full_Name())
+# print(my_tesla.fuel_type())
+ElectricCar("Tesla" , "Model A", "85KWH")
+Car("Bugati", "1212")
+# print(Car.fuel_type())
+# print(Car.total_car)
+#  print(my_car.full_Name())
+print(isinstance(my_tesla,Car))
+print(isinstance(my_tesla,ElectricCar))
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
